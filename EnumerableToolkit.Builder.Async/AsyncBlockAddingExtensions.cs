d5b1fd9d38723bd7965333152826e30cb6974d69
Arne Kiesewetter <banane9@live.de>
2024-08-04 00:21:15 +0200

    Add IEnumerable overloads to Block Adding Extensions

--- EnumerableToolkit.Builder.Async/AsyncBlockAddingExtensions.cs
+++ EnumerableToolkit.Builder.Async/AsyncBlockAddingExtensions.cs
@@ -29,6 +29,15 @@ namespace EnumerableToolkit.Builder
             => builder.AddBuildingBlock(new AsyncConcatBlock<T>(sequence));
 
         /// <summary>
+        /// Adds a building block that concatenates the given sequence to the end of the current constructed enumerable sequence.
+        /// </summary>
+        /// <typeparam name="T">The type of the items in the generated sequence.</typeparam>
+        /// <param name="builder">The enumerable builder to add the building block to.</param>
+        /// <param name="sequence">The sequence to concatenate.</param>
+        public static void Concat<T>(this AsyncEnumerableBuilder<T> builder, IEnumerable<T> sequence)
+            => builder.AddBuildingBlock(new AsyncConcatBlock<T>(sequence.ToAsyncEnumerable()));
+
+        /// <summary>
         /// Adds a building block that inserts the given items after every item that matches the given <paramref name="predicate"/>.
         /// </summary>
         /// <typeparam name="T">The type of the items in the generated sequence.</typeparam>
@@ -48,6 +57,16 @@ namespace EnumerableToolkit.Builder
         /// <param name="sequence">A sequence of items that should be added.</param>
         public static void InsertAfterEveryItem<T>(this AsyncEnumerableBuilder<T> builder, Func<T, int, bool> predicate, IAsyncEnumerable<T> sequence)
             => builder.AddBuildingBlock(new AsyncInsertAfterEveryItemLambdaBlock<T>(predicate, sequence));
+
+        /// <summary>
+        /// Adds a building block that inserts the given sequence after every item that matches the given <paramref name="predicate"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the items in the generated sequence.</typeparam>
+        /// <param name="builder">The enumerable builder to add the building block to.</param>
+        /// <param name="predicate">A predicate determining whether the <paramref name="sequence"/> of items should be inserted after an item.</param>
+        /// <param name="sequence">A sequence of items that should be added.</param>
+        public static void InsertAfterEveryItem<T>(this AsyncEnumerableBuilder<T> builder, Func<T, int, bool> predicate, IEnumerable<T> sequence)
+            => builder.AddBuildingBlock(new AsyncInsertAfterEveryItemLambdaBlock<T>(predicate, sequence.ToAsyncEnumerable()));
 
         /// <summary>
         /// Adds a building block that inserts the given items after every item that matches the given <paramref name="predicate"/>.
@@ -71,6 +90,16 @@ namespace EnumerableToolkit.Builder
             => builder.InsertAfterEveryItem(predicate.Wrap(), sequence);
 
         /// <summary>
+        /// Adds a building block that inserts the given sequence after every item that matches the given <paramref name="predicate"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the items in the generated sequence.</typeparam>
+        /// <param name="builder">The enumerable builder to add the building block to.</param>
+        /// <param name="predicate">A predicate determining whether the <paramref name="sequence"/> of items should be inserted after an item.</param>
+        /// <param name="sequence">A sequence of items that should be added.</param>
+        public static void InsertAfterEveryItem<T>(this AsyncEnumerableBuilder<T> builder, Func<T, bool> predicate, IEnumerable<T> sequence)
+            => builder.InsertAfterEveryItem(predicate.Wrap(), sequence.ToAsyncEnumerable());
+
+        /// <summary>
         /// Adds a building block that inserts the given items after the first item that matches the given <paramref name="predicate"/>.
         /// </summary>
         /// <typeparam name="T">The type of the items in the generated sequence.</typeparam>
@@ -90,6 +119,16 @@ namespace EnumerableToolkit.Builder
         /// <param name="sequence">A sequence of items that should be added.</param>
         public static void InsertAfterFirstItem<T>(this AsyncEnumerableBuilder<T> builder, Func<T, int, bool> predicate, IAsyncEnumerable<T> sequence)
             => builder.AddBuildingBlock(new AsyncInsertAfterFirstItemLambdaBlock<T>(predicate, sequence));
+
+        /// <summary>
+        /// Adds a building block that inserts the given sequence after the first item that matches the given <paramref name="predicate"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the items in the generated sequence.</typeparam>
+        /// <param name="builder">The enumerable builder to add the building block to.</param>
+        /// <param name="predicate">A predicate determining whether the <paramref name="sequence"/> of items should be inserted after that item.</param>
+        /// <param name="sequence">A sequence of items that should be added.</param>
+        public static void InsertAfterFirstItem<T>(this AsyncEnumerableBuilder<T> builder, Func<T, int, bool> predicate, IEnumerable<T> sequence)
+            => builder.AddBuildingBlock(new AsyncInsertAfterFirstItemLambdaBlock<T>(predicate, sequence.ToAsyncEnumerable()));
 
         /// <summary>
         /// Adds a building block that inserts the given items after the first item that matches the given <paramref name="predicate"/>.
@@ -113,6 +152,16 @@ namespace EnumerableToolkit.Builder
             => builder.InsertAfterFirstItem(predicate.Wrap(), sequence);
 
         /// <summary>
+        /// Adds a building block that inserts the given sequence after the first item that matches the given <paramref name="predicate"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the items in the generated sequence.</typeparam>
+        /// <param name="builder">The enumerable builder to add the building block to.</param>
+        /// <param name="predicate">A predicate determining whether the <paramref name="sequence"/> of items should be inserted after that item.</param>
+        /// <param name="sequence">A sequence of items that should be added.</param>
+        public static void InsertAfterFirstItem<T>(this AsyncEnumerableBuilder<T> builder, Func<T, bool> predicate, IEnumerable<T> sequence)
+            => builder.InsertAfterFirstItem(predicate.Wrap(), sequence.ToAsyncEnumerable());
+
+        /// <summary>
         /// Adds a building block that inserts the given items before every item that matches the given <paramref name="predicate"/>.
         /// </summary>
         /// <typeparam name="T">The type of the items in the generated sequence.</typeparam>
@@ -132,6 +181,16 @@ namespace EnumerableToolkit.Builder
         /// <param name="sequence">A sequence of items that should be added.</param>
         public static void InsertBeforeEveryItem<T>(this AsyncEnumerableBuilder<T> builder, Func<T, int, bool> predicate, IAsyncEnumerable<T> sequence)
             => builder.AddBuildingBlock(new AsyncInsertBeforeEveryItemLambdaBlock<T>(predicate, sequence));
+
+        /// <summary>
+        /// Adds a building block that inserts the given sequence before every item that matches the given <paramref name="predicate"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the items in the generated sequence.</typeparam>
+        /// <param name="builder">The enumerable builder to add the building block to.</param>
+        /// <param name="predicate">A predicate determining whether the <paramref name="sequence"/> of items should be inserted before an item.</param>
+        /// <param name="sequence">A sequence of items that should be added.</param>
+        public static void InsertBeforeEveryItem<T>(this AsyncEnumerableBuilder<T> builder, Func<T, int, bool> predicate, IEnumerable<T> sequence)
+            => builder.AddBuildingBlock(new AsyncInsertBeforeEveryItemLambdaBlock<T>(predicate, sequence.ToAsyncEnumerable()));
 
         /// <summary>
         /// Adds a building block that inserts the given items before every item that matches the given <paramref name="predicate"/>.
@@ -155,6 +214,16 @@ namespace EnumerableToolkit.Builder
             => builder.InsertBeforeEveryItem(predicate.Wrap(), sequence);
 
         /// <summary>
+        /// Adds a building block that inserts the given sequence before every item that matches the given <paramref name="predicate"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the items in the generated sequence.</typeparam>
+        /// <param name="builder">The enumerable builder to add the building block to.</param>
+        /// <param name="predicate">A predicate determining whether the <paramref name="sequence"/> of items should be inserted before an item.</param>
+        /// <param name="sequence">A sequence of items that should be added.</param>
+        public static void InsertBeforeEveryItem<T>(this AsyncEnumerableBuilder<T> builder, Func<T, bool> predicate, IEnumerable<T> sequence)
+            => builder.InsertBeforeEveryItem(predicate.Wrap(), sequence.ToAsyncEnumerable());
+
+        /// <summary>
         /// Adds a building block that inserts the given items before the first item that matches the given <paramref name="predicate"/>.
         /// </summary>
         /// <typeparam name="T">The type of the items in the generated sequence.</typeparam>
@@ -176,6 +245,16 @@ namespace EnumerableToolkit.Builder
             => builder.AddBuildingBlock(new AsyncInsertBeforeFirstItemLambdaBlock<T>(predicate, sequence));
 
         /// <summary>
+        /// Adds a building block that inserts the given sequence before the first item that matches the given <paramref name="predicate"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the items in the generated sequence.</typeparam>
+        /// <param name="builder">The enumerable builder to add the building block to.</param>
+        /// <param name="predicate">A predicate determining whether the <paramref name="sequence"/> of items should be inserted before that item.</param>
+        /// <param name="sequence">A sequence of items that should be added.</param>
+        public static void InsertBeforeFirstItem<T>(this AsyncEnumerableBuilder<T> builder, Func<T, int, bool> predicate, IEnumerable<T> sequence)
+            => builder.AddBuildingBlock(new AsyncInsertBeforeFirstItemLambdaBlock<T>(predicate, sequence.ToAsyncEnumerable()));
+
+        /// <summary>
         /// Adds a building block that inserts the given items before the first item that matches the given <paramref name="predicate"/>.
         /// </summary>
         /// <typeparam name="T">The type of the items in the generated sequence.</typeparam>
@@ -195,6 +274,16 @@ namespace EnumerableToolkit.Builder
         /// <param name="sequence">A sequence of items that should be added.</param>
         public static void InsertBeforeFirstItem<T>(this AsyncEnumerableBuilder<T> builder, Func<T, bool> predicate, IAsyncEnumerable<T> sequence)
             => builder.InsertBeforeFirstItem(predicate.Wrap(), sequence);
+
+        /// <summary>
+        /// Adds a building block that inserts the given sequence before the first item that matches the given <paramref name="predicate"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the items in the generated sequence.</typeparam>
+        /// <param name="builder">The enumerable builder to add the building block to.</param>
+        /// <param name="predicate">A predicate determining whether the <paramref name="sequence"/> of items should be inserted before that item.</param>
+        /// <param name="sequence">A sequence of items that should be added.</param>
+        public static void InsertBeforeFirstItem<T>(this AsyncEnumerableBuilder<T> builder, Func<T, bool> predicate, IEnumerable<T> sequence)
+            => builder.InsertBeforeFirstItem(predicate.Wrap(), sequence.ToAsyncEnumerable());
 
         /// <summary>
         /// Adds a building block that filters the current sequence based on the given <paramref name="predicate"/>.
