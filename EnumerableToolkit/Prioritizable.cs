42bcdda081e815336472dc768ffe3394a214ae5d
Arne Kiesewetter <banane9@live.de>
2024-08-24 01:24:01 +0200

    Add Prioritizables and PrioritySortedCollection using them

---
+++ EnumerableToolkit/Prioritizable.cs
@@ -0,0 +1,35 @@
+﻿namespace EnumerableToolkit
+{
+    /// <summary>
+    /// Contains a comparer for <see cref="IPrioritizable">prioritizable</see> things.
+    /// </summary>
+    public static class Prioritizable
+    {
+        /// <summary>
+        /// Gets an <see cref="IComparer{T}"/> that sorts <see cref="IPrioritizable"/> instances
+        /// by their <see cref="IPrioritizable.Priority">Priority</see> in descending order.
+        /// </summary>
+        public static IComparer<IPrioritizable> Comparer { get; } = new PrioritizableComparer();
+
+        private sealed class PrioritizableComparer : IComparer<IPrioritizable>
+        {
+            public int Compare(IPrioritizable x, IPrioritizable y)
+                => Comparer<int>.Default.Compare(y.Priority, x.Priority);
+        }
+    }
+
+    /// <summary>
+    /// Defines the interface for prioritizable things.
+    /// </summary>
+    public interface IPrioritizable
+    {
+        /// <summary>
+        /// Gets the priority of this item. Use an agreed standard as a base.
+        /// </summary>
+        /// <value>
+        /// An interger used to sort the prioritizable items.<br/>
+        /// Higher comes first with the <see cref="Prioritizable.Comparer">default comparer</see>.
+        /// </value>
+        public int Priority { get; }
+    }
+}
---
+++ EnumerableToolkit/PrioritySortedCollection.cs
@@ -0,0 +1,30 @@
+﻿namespace EnumerableToolkit
+{
+    /// <summary>
+    /// Represents a collection which is kept sorted using the items' <see cref="IPrioritizable.Priority"/>
+    /// using the <see cref="Prioritizable.Comparer"/> (higher sorts first).
+    /// </summary>
+    /// <remarks>
+    /// Duplicate items, and multiple items comparing equal are supported.
+    /// </remarks>
+    /// <inheritdoc/>
+    public class PrioritySortedCollection<T> : SortedCollection<T>
+        where T : notnull, IPrioritizable
+    {
+        /// <summary>
+        /// Constructs an empty sorted collection using the items' <see cref="IPrioritizable.Priority"/> for sorting.
+        /// </summary>
+        public PrioritySortedCollection()
+            : base((IComparer<T>)Prioritizable.Comparer)
+        { }
+
+        /// <summary>
+        /// Constructs a sorted collection with the given elements,
+        /// while using the items' <see cref="IPrioritizable.Priority"/> for sorting.
+        /// </summary>
+        /// <param name="collection">The elements to add to the collection.</param>
+        public PrioritySortedCollection(IEnumerable<T>? collection)
+            : base(collection, (IComparer<T>)Prioritizable.Comparer)
+        { }
+    }
+}
